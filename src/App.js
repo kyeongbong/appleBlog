47dd9056676de0a3e4a601e3891a01a2f2ac70bf
kyeongbong <kt03170@gmail.com>
2022-03-24 21:33:02 +0900

    modalmake

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 function App() {
   let [글제목, 글제목변경] = useState([
     "남자 코디 추천",
-    "익산 순대국 맛짐",
+    "익산 순대국 맛집",
     "잠깨는법",
   ]);
 
   let [like, likes] = useState(0);
 
+  let [modal, modal변경] = useState(1);
+
   return (
     <div className="App">
       <div className="black_nav">개발 Blog</div>
       <div className="list">
-        <button
+        {/* <button
           onClick={() => {
             글제목변경(
-              (글제목 = ["잠깨는법", "익산 순대국 맛짐", "남자 코디 추천"])
+              (글제목 = ["잠깨는법", "익산 순대국 맛집", "남자 코디 추천"])
             );
           }}
         >
           제목바꾸기
-        </button>
+        </button> */}
         <h3>
           {글제목[0]}{" "}
           <span
@@ -46,10 +48,36 @@ function App() {
       </div>
 
       <div className="list">
-        <h3>{글제목[2]}</h3>
+        <h3
+          onClick={() => {
+            modal변경(modal + 1);
+          }}
+        >
+          {글제목[2]}
+        </h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
+
+      <button
+        onClick={() => {
+          modal변경(modal + 1);
+        }}
+      >
+        Modal
+      </button>
+
+      {modal % 2 === 0 ? <Modal></Modal> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
